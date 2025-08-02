5c05f500bc23b9a89bc7a8b44a41e0cae164b8c8
mcmanyika <mcmanyika@gmail.com>
2025-08-02 09:19:53 -0400

    first commit

--- src/config/contractAddresses.js
+++ src/config/contractAddresses.js
@@ -4,9 +4,7 @@ const CONTRACT_ADDRESS = {
   4: 'YOUR_RINKEBY_ADDRESS', // Rinkeby
   5: 'YOUR_GOERLI_ADDRESS',  // Goerli
   42: 'YOUR_KOVAN_ADDRESS',  // Kovan
-  5777: '0x25aC392418AbB935C9401Ca53263d2aC767756A7', // Local Ganache network (latest)
-  //5777: '0xD2a11bD3d48daDa994cdFC9076430a4f2e992691', // Local Ganache network (latest)
-  // 5777: '0xB4CcBc164b0CC17881612049f4d91A00b6136d29' // Previous Ganache network
+  5777: '0xE20677F28c03F92Ff84C76BC8AF419a6e2D9D6e3', // Local Ganache network (latest)
 };
 
 export default CONTRACT_ADDRESS; 
