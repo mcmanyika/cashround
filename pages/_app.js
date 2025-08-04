9bc9847553354d7c852f57a64c4c8bcbbcacd469
mcmanyika <mcmanyika@gmail.com>
2025-08-04 15:11:48 -0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,6 @@ import '../src/components/styles/SendToReferrers.css';
 import '../src/components/styles/UserDashboard.css';
 import '../src/components/layout/Layout.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { ThirdwebProvider } from '@thirdweb-dev/react';
-import { client } from '../src/thirdwebClient';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -18,9 +16,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="Connect with your referrer and start earning rewards" />
       </Head>
-      <ThirdwebProvider client={client}>
         <Component {...pageProps} />
-      </ThirdwebProvider>
     </>
   );
 }
